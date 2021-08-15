62441c181ced4d59996fa6e359bc6ac59b9ae2b9
Nikita1814 <kravchenko1814@gmail.com>
2021-08-15 21:18:58 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length < 0) {
+        return 0;
+    }
+    let minNum = 0;
+    for (i = 0; i < array.length; i++) {
+        if (array[i] < minNum) {
+            minNum = array[i];
+        }
+    }
+    return minNum;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length < 0) {
+        return 0;
+    }
+    let maxNum = 0;
+    for (i = 0; i < array.length; i++) {
+        if (array[i] > maxNum) {
+            maxNum = array[i];
+        }
+    }
+    return maxNum;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length <= 0) {
+        return 0;
+    } else {
+        let sum = array.reduce((acc, val) => acc + val);
+        return sum / array.length;
+    }
+};
